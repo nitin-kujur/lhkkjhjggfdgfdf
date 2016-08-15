fdc2ac706f1ab3c832d9080899fa924429c66b03
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-08-15 11:50:50 +0530

    Updated order controller action to resolve issue

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
    include ShopifyApp::AppProxyVerification
 
   def index
-  	@products = ShopifyAPI::Product.find(:all)
     render layout: false, content_type: 'application/liquid'
   end
 
