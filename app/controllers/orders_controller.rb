aba330cd03fe6f96da09b5141f0836cdb768491e
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-08-21 19:10:39 +0530

    Updated order controller to add order create api

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,49 @@ class OrdersController <  ShopifyApp::AuthenticatedController
    #include ShopifyApp::AppProxyVerification
 
   def index
-    # render layout: false, content_type: 'application/liquid'
+    @orders = ShopifyAPI::Order.find(:all, limit: 10)
   end
 
   def place_bulk_order
-    byebug
   	# @distibutors = ShopifyAPI::Customer.where(id: session[:bulk_order]['distributor'].keys)
   end
 
 	def bulk_order
-    render text: params and return false
+    params[:order][:distributor].each do |cust_id, values|
+      customer = ShopifyAPI::Customer.find(cust_id)
+      line_items = []
+      default_address = customer.default_address
+      address = customer.addresses.first
+      billing_address = {first_name: default_address.first_name, 
+                        last_name: default_address.last_name, 
+                        address1: default_address.address1,
+                        phone: default_address.phone,
+                        city: default_address.city,
+                        province: default_address.province,
+                        country: default_address.country,
+                        zip: default_address.zip}
+      shipping_address = {first_name: address.first_name, 
+                        last_name: address.last_name, 
+                        address1: address.address1,
+                        phone: address.phone,
+                        city: address.city,
+                        province: address.province,
+                        country: address.country,
+                        zip: address.zip}
+      financial_status = 'partially_paid'
+      values[:product].each do |product_id, p_values|
+        if p_values[:quantity].present? && p_values[:quantity].to_i > 0
+          product = ShopifyAPI::Product.find(product_id)
+          variant = product.variants.first
+          line_items << {variant_id: variant.id, quantity: p_values[:quantity]}
+        end
+      end
+      if line_items.present?
+        order = ShopifyAPI::Order.new(line_items: line_items, customer: {id: customer.id}, billing_address: billing_address, shipping_address: shipping_address, financial_status: financial_status)
+        order.save
+      end
+    end
+    redirect_to orders_path, notice: 'Bulk Orders have been successfully created.'
   end
 
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV["shopify_api_key"]
   config.secret = ENV["shopify_secret"]
-  config.scope = "read_orders, read_products, read_customers, write_customers"
+  config.scope = "write_orders, read_orders, read_products, read_customers, write_customers"
   config.embedded_app = true
 end
