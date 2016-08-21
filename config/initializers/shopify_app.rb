5a234837c32085e027babd70e8db7cd4e79c9d3f
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-08-21 19:14:37 +0530

    Updated shopify configuration to set view

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,5 +2,5 @@ ShopifyApp.configure do |config|
   config.api_key = ENV["shopify_api_key"]
   config.secret = ENV["shopify_secret"]
   config.scope = "write_orders, read_orders, read_products, read_customers, write_customers"
-  config.embedded_app = true
+  config.embedded_app = false
 end
