a02108d0546a6faedb677a16823480a695fe46cc
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-08-26 11:36:55 +0530

    Updated shopify api permission to shipping rate

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV["shopify_api_key"]
   config.secret = ENV["shopify_secret"]
-  config.scope = "write_orders, read_orders, read_products, read_customers, write_customers"
+  config.scope = "write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
   config.embedded_app = false
 end
