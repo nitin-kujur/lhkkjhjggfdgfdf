05476752912e8562b54c09e5b2068148ec2940f0
Arpit Vaishnav <arpitvaishnav@gmail.com>
2016-08-28 13:56:22 +0530

    updated production rb for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.action_controller.asset_host = 'shopify-bulk-order.herokuapp.com'
-  Rails.application.routes.default_url_options[:host] = 'shopify-bulk-order.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'shopify-bulk-order.herokuapp.com' }
   
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
