b78be6fe1c82b101da52ebb1fd4c2077e53fb022
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-08-28 14:58:24 +0530

    updated view

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -9,16 +9,12 @@ class AppProxyController < ApplicationController
   private
   
     def set_session
-      if session[:shopify].blank?
-        if params[:shop].present?
-          shop = Shop.find_by_shopify_domain(params[:shop])
-          if shop.present?
-            sess = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
-            session[:shopify] = ShopifyApp::SessionRepository.store(sess)
-            ShopifyAPI::Base.activate_session(sess)
-            session[:shopify_domain] = shop.shopify_domain
-          end
-        end
+      shop = Shop.find_by_shopify_domain(params[:shop])
+      if shop.present?
+        sess = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
+        session[:shopify] = ShopifyApp::SessionRepository.store(sess)
+        ShopifyAPI::Base.activate_session(sess)
+        session[:shopify_domain] = shop.shopify_domain
       end
     end
 end
