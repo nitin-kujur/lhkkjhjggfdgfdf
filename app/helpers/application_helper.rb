ea6e7b0a9f0b9b5a0cf674eb95d705581abb264d
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-08-28 10:47:45 +0530

    updated view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,20 @@
+
 module ApplicationHelper
 
 	def get_shipping_options
 		shipping_options = []
-		ss = ShopifyAPI::ShippingZone.find(:all)
-		ss.each do |s|
-			shipping_options << 'Base On Weight' if s.weight_based_shipping_rates.present?
-			shipping_options << 'Base On Price' if s.price_based_shipping_rates.present?
-			s.carrier_shipping_rate_providers.each do |c|
-				shipping_options += c.service_filter.attributes.select{|s,v| v=='+' && s !='*'}.keys
+		begin
+			ss = ShopifyAPI::ShippingZone.find(:all)
+			ss.each do |s|
+				shipping_options << 'Base On Weight' if s.weight_based_shipping_rates.present?
+				shipping_options << 'Base On Price' if s.price_based_shipping_rates.present?
+				s.carrier_shipping_rate_providers.each do |c|
+					shipping_options += c.service_filter.attributes.select{|s,v| v=='+' && s !='*'}.keys
+				end
 			end
-		end
+	  rescue => ex
+	    shipping_options << ex.message
+	  end
 		shipping_options.uniq
 	end
 end
