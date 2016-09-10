84c2d238e2022661a6916cb1612261c7f85940a2
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-10 18:01:03 +0530

    updated shopify configuration

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.api_key = ENV["shopify_api_key"]
   config.secret = ENV["shopify_secret"]
   config.scope = "write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
-  config.embedded_app = false
+  config.embedded_app = true
 end
 
 SITE_URL = 'https://shopify-bulk-order.herokuapp.com'
