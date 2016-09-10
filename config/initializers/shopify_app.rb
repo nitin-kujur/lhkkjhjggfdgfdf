019c829214df16a4608016995b790f6c1758c386
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-10 22:45:06 +0530

    updated scope of shopify

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV["shopify_api_key"]
   config.secret = ENV["shopify_secret"]
-  config.scope = "write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
+  config.scope = "read_script_tags, write_script_tags, write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
   config.embedded_app = true	
   config.webhooks = [
     {topic: 'carts/create', address: 'https://shopify-bulk-order.herokuapp.com/webhooks/carts_update', format: "json"}
