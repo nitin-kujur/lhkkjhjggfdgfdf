4e83bf3eb44da420ddf4b86b480db500d8127664
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-11 13:00:40 +0530

    updated configuration file

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,10 +4,10 @@ ShopifyApp.configure do |config|
   config.scope = "read_script_tags, write_script_tags, write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
   config.embedded_app = true	
   config.webhooks = [
-    {topic: 'carts/create', address: 'https://shopify-bulk-order.herokuapp.com/webhooks/carts_update', format: "json"}
+    {topic: 'carts/create', address: "#{ENV['shopify_secret']}/webhooks/carts_update", format: "json"}
   ]
   config.scripttags = [
-    {event:'onload', src: 'https://shopify-bulk-order.herokuapp.com/webhooks/carts_update'}
+    {event:'onload', src: "#{ENV['shopify_secret']}/webhooks/carts_update"}
   ]
 end
 SITE_URL = 'https://shopify-bulk-order.herokuapp.com'
