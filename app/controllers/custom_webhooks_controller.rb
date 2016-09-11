8f95e428e1ba8b46608321daced55899924c74c4
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-11 17:08:21 +0530

    updated webook

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -2,6 +2,7 @@ class CustomWebhooksController < ApplicationController
   # include ShopifyApp::WebhookVerification
   layout false
   def carts_update
+  	puts params.inspect
     respond_to do |format|
       format.html { }
       format.json { }
