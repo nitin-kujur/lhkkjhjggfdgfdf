6262d581fc699fc12af2f369be3bd668f6a642e8
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-13 01:55:04 +0530

    udpated

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -4,7 +4,7 @@ class CustomWebhooksController < ApplicationController
   def carts_update
   	puts params.inspect
   	params[:line_items].each do |s|
-  		line_item = ShopifyApp::LineItem.find(s['id'])
+  		line_item = ShopifyAPI::LineItem.find(s['id'])
   		line_item.price = 20.00
   		line_item.save
   	end
