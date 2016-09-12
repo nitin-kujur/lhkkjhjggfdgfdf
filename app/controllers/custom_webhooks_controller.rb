68e1e2b9759c6cf147672e29149d02a71ed4cffc
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-13 02:02:58 +0530

    udpated

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -1,4 +1,5 @@
 class CustomWebhooksController < ApplicationController
+	before_action :set_session
   include ShopifyApp::WebhookVerification
 
   def carts_update
@@ -12,6 +13,18 @@ class CustomWebhooksController < ApplicationController
       format.html { }
       format.json { }
       format.js {}
+    end
+  end
+  def set_session
+    params[:shop] = 'pepsi-test.myshopify.com' if params[:shop].blank? || Rails.env.development?
+    if params[:shop].present?
+      shop = Shop.find_by_shopify_domain(params[:shop])
+      if shop.present?
+        sess = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
+        session[:shopify] = ShopifyApp::SessionRepository.store(sess)
+        ShopifyAPI::Base.activate_session(sess)
+        session[:shopify_domain] = shop.shopify_domain
+      end
     end
   end
 end
