ca03473524292a0f07aab58483d9a80cf6a6610c
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-14 08:44:07 +0530

    Updated

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -4,11 +4,6 @@ class CustomWebhooksController < ApplicationController
 
   def carts_update
   	puts params.inspect
-  	params[:line_items].each do |s|
-  		line_item = ShopifyAPI::LineItem.find(s['id'])
-  		line_item.price = 20.00
-  		line_item.save
-  	end
     respond_to do |format|
       format.html { }
       format.json { }
