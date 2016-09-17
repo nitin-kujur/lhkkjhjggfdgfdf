b6920d9906cb58477c06244543ecab83ece032e7
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-17 10:20:06 +0530

    Updated configuration to set amazon settings

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,11 +4,11 @@ ShopifyApp.configure do |config|
   config.scope = "read_script_tags, write_script_tags, write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
   config.embedded_app = true	
   config.webhooks = [
-    {topic: 'carts/create', address: "https://shopify-bulk-order.herokuapp.com/carts_update", format: "json"},
-    {topic: 'carts/update', address: "https://shopify-bulk-order.herokuapp.com/carts_update", format: "json"}
+    {topic: 'carts/create', address: "https://http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com/carts_update", format: "json"},
+    {topic: 'carts/update', address: "https://http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com/carts_update", format: "json"}
   ]
   config.scripttags = [
-    {event:'onload', src: "https://shopify-bulk-order.herokuapp.com/update_quantity"}
+    {event:'onload', src: "http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com/update_quantity"}
   ]
 end
-SITE_URL = 'https://shopify-bulk-order.herokuapp.com'
+SITE_URL = 'http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com'
