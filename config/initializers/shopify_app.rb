f5dd7f341e4e1e52c9d54110a82e53300f37036a
Kaushal Kishor Sharma <kaushalk16@gmail.com>
2016-09-17 14:45:06 +0530

    Remove api call back

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,12 +3,5 @@ ShopifyApp.configure do |config|
   config.secret = ENV["shopify_secret"]
   config.scope = "read_script_tags, write_script_tags, write_orders, read_orders, read_products, read_customers, write_customers, write_shipping"
   config.embedded_app = true	
-  config.webhooks = [
-    {topic: 'carts/create', address: "https://http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com/carts_update", format: "json"},
-    {topic: 'carts/update', address: "https://http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com/carts_update", format: "json"}
-  ]
-  config.scripttags = [
-    {event:'onload', src: "http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com/update_quantity"}
-  ]
 end
 SITE_URL = 'http://ec2-52-88-152-46.us-west-2.compute.amazonaws.com'
